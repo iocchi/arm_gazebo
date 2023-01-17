ade297df5110cada15e01e00d546c55b7b4bfe66
iocchi <iocchi@diag.uniroma1.it>
2023-01-17 16:21:27 +0100

    cartesian_impedance_example_controller; no moveit

--- bin/init_sim_panda.bash
+++ bin/init_sim_panda.bash
@@ -9,15 +9,14 @@ tmux send-keys -t $SESSION:0 "roslaunch arm_gazebo arm_panda.launch world:=`rosp
 
 sleep 15
 
-tmux new-window -t $SESSION:1 -n 'moveit'
-tmux send-keys -t $SESSION:1 "roslaunch panda_moveit_config demo.launch load_robot_description:=false  moveit_controller_manager:=simple use_rviz:=false"  C-m
+# tmux new-window -t $SESSION:1 -n 'moveit'
+# tmux send-keys -t $SESSION:1 "roslaunch panda_moveit_config demo.launch load_robot_description:=false  moveit_controller_manager:=simple use_rviz:=false"  C-m
 
+# sleep 5
 
-sleep 5
-
-tmux new-window -t $SESSION:2 -n 'rviz'
-tmux send-keys -t $SESSION:2 "roscd panda_moveit_config/launch" C-m
-tmux send-keys -t $SESSION:2 "rosrun rviz rviz -d moveit.rviz"
+# tmux new-window -t $SESSION:2 -n 'rviz'
+# tmux send-keys -t $SESSION:2 "roscd panda_moveit_config/launch" C-m
+# tmux send-keys -t $SESSION:2 "rosrun rviz rviz -d moveit.rviz"
 
 
 tmux new-window -t $SESSION:3 -n 'scripts'
